1684952ea6ad513bf139c144f24b8174bd81a5a1
Luis <84397555+Luisin13@users.noreply.github.com>
2022-12-09 19:09:06 -0300

    update

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # My dotfiles
 
 Currently working on automating all Windows environment settings like Git, Powershell, VS Code and more!
+
+There are other apps, but they work best when installing them from Microsoft Store, like:
+- PowerToys
+- ModernFlyouts
--- setup.ps1
+++ setup.ps1
@@ -5,18 +5,15 @@ winget install -e -id Microsoft.PowerShell
 winget install -e --id Git.Git
 winget install -e --id GitHub.GitHubDesktop
 
-#Install Node
-winget install -e --id OpenJS.NodeJS.LTS -v 16.14.0
+#Install NVM
+winget install -e --id CoreyButler.NVMforWindows
 
 #Install Python
-winget install -e --id Python.Python.3 -v 3.10.2150
+winget install -e --id Python.Python.3.10 -v 3.10.8
 
 #Install Discord
 winget install -e --id discord.Discord
 winget install -e --id discord.Discord.PTB
-
-#Install Yarn
-winget install -e --id Yarn.Yarn
 
 #Install VS Code
 winget install -e --id Microsoft.VisualStudioCode
@@ -30,9 +27,42 @@ winget install -e --id Microsoft.WindowsTerminal
 #Install Scoop
 iwr -useb get.scoop.sh | iex
 
-#Instal Curl and NeoVim
+#Install Curl and NeoVim
 scoop install curl neovim
 
+#Install winrar
+winget install -e --id RARLab.WinRAR
+
+#Install cosmetics
+winget install -e --id Rainmeter.Rainmeter
+winget install -e --id rocksdanister.LivelyWallpaper
+winget install -e --id 9PF4KZ2VN4W9 --source msstore
+
+#Install gaming things
+winget install -e --id Valve.Steam
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id Spotify.Spotify
+winget install -e --id Logitech.GHUB
+
 #Install terminal theme engine
-Install-Module posh-git -Scope CurrentUser - Force
-Install-Module oh-my-posh -Scope CurrentUser -Force
+winget install -e --id JanDeDobbeleer.OhMyPosh -s winget
+Install-Module posh-git -Scope CurrentUser -Force
+Install-Module Terminal-Icons -Scope CurrentUser -Force
+
+#Install extra terminal features
+Install-Module PSReadLine -Scope CurrentUser -Force
+
+#Install Node.js
+nvm install 16.14.0
+nvm on
+nvm use 16.14.0
+npm install -g yarn
+
+#Install WSL
+wsl --install
+wsl --install -d Ubuntu
+wsl --set-default Ubuntu
+wsl --set-version Ubuntu 2
+
+#Install Docker
+winget install -e --id Docker.DockerDesktop
