f60801a54461d4c1701f38d4551e11ce2640ed7d
Luis <84397555+LuisFadini@users.noreply.github.com>
2026-06-03 13:13:48 -0300

    Update Matugen config for newer version

--- .config/hypr/colors.lua
+++ .config/hypr/colors.lua
@@ -1,103 +1,104 @@
 return {
-	image = "/home/luis/Pictures/wallpapers/rosepeak.png",
-
+	image = "/home/luis/dotfiles/Pictures/wallpapers/rosepeak.png",
+	
 	background = "#121415ff",
-
+	
 	error = "#ffb4abff",
-
+	
 	error_container = "#93000aff",
-
+	
 	inverse_on_surface = "#2f3132ff",
-
+	
 	inverse_primary = "#3a6475ff",
-
+	
 	inverse_surface = "#e2e2e4ff",
-
+	
 	on_background = "#e2e2e4ff",
-
+	
 	on_error = "#690005ff",
-
+	
 	on_error_container = "#ffdad6ff",
-
+	
 	on_primary = "#013545ff",
-
+	
 	on_primary_container = "#ffffffff",
-
+	
 	on_primary_fixed = "#001f2aff",
-
+	
 	on_primary_fixed_variant = "#214c5dff",
-
+	
 	on_secondary = "#243239ff",
-
+	
 	on_secondary_container = "#d6e6efff",
-
+	
 	on_secondary_fixed = "#0e1d24ff",
-
+	
 	on_secondary_fixed_variant = "#3a4950ff",
-
+	
 	on_surface = "#e2e2e4ff",
-
+	
 	on_surface_variant = "#c1c7ccff",
-
+	
 	on_tertiary = "#3d2848ff",
-
+	
 	on_tertiary_container = "#ffffffff",
-
+	
 	on_tertiary_fixed = "#271332ff",
-
+	
 	on_tertiary_fixed_variant = "#543e60ff",
-
+	
 	outline = "#8b9296ff",
-
+	
 	outline_variant = "#41484bff",
-
+	
 	primary = "#a3cde1ff",
-
+	
 	primary_container = "#4a7385ff",
-
+	
 	primary_fixed = "#bee9feff",
-
+	
 	primary_fixed_dim = "#a3cde1ff",
-
+	
 	scrim = "#000000ff",
-
+	
 	secondary = "#b9c9d2ff",
-
+	
 	secondary_container = "#3c4b53ff",
-
+	
 	secondary_fixed = "#d5e5eeff",
-
+	
 	secondary_fixed_dim = "#b9c9d2ff",
-
+	
 	shadow = "#000000ff",
-
+	
 	source_color = "#4a7385ff",
-
+	
 	surface = "#121415ff",
-
+	
 	surface_bright = "#37393bff",
-
+	
 	surface_container = "#1e2021ff",
-
+	
 	surface_container_high = "#282a2bff",
-
+	
 	surface_container_highest = "#333536ff",
-
+	
 	surface_container_low = "#1a1c1dff",
-
+	
 	surface_container_lowest = "#0c0e10ff",
-
+	
 	surface_dim = "#121415ff",
-
+	
 	surface_tint = "#a3cde1ff",
-
+	
 	surface_variant = "#41484bff",
-
+	
 	tertiary = "#d9bde5ff",
-
+	
 	tertiary_container = "#7d6589ff",
-
+	
 	tertiary_fixed = "#f5d9ffff",
-
+	
 	tertiary_fixed_dim = "#d9bde5ff",
+	
 }
